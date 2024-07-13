013dea54500ed7c6a4a53ee989091597d1da51a9
ISHITA NIGAM <ishitanigam800@gmail.com>
2024-07-13 19:49:14 +0530

    Add 22 Day program

---
+++ DSA/Ishita-Nigam/Day22/Day22_Q2.java
@@ -0,0 +1,32 @@
+import java.util.Stack;
+
+public class Day22_Q2 {
+    public String reverseParentheses(String s) {
+        Stack<Integer> stack = new Stack<>();
+        StringBuilder sb = new StringBuilder();
+        for (char ch : s.toCharArray()) {
+            if (ch == '(') {
+                stack.push(sb.length());
+            } else if (ch == ')') {
+                int start = stack.pop();
+                reverse(sb, start, sb.length() - 1);
+            } else {
+                sb.append(ch);
+            }
+        }
+        return sb.toString();
+    }
+
+    public void reverse(StringBuilder rev, int start, int end) {
+        while (start < end) {
+            char temp = rev.charAt(start);
+            rev.setCharAt(start++, rev.charAt(end));
+            rev.setCharAt(end--, temp);
+        }
+    }
+
+    public static void main(String[] args) {
+        Day22_Q2 solution = new Day22_Q2();
+        System.out.println(solution.reverseParentheses("(u(love)i)")); // Example usage
+    }
+}
