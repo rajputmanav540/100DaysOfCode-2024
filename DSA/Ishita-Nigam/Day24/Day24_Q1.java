38b6b69c17e2bd18176a158338a3fd0478e8018c
ISHITA NIGAM <ishitanigam800@gmail.com>
2024-07-15 10:17:41 +0530

    Add 24 Day program

---
+++ DSA/Ishita-Nigam/Day24/Day24_Q1.java
@@ -0,0 +1,30 @@
+public class Day24_Q1 {
+    public int[] sortedSquares(int[] arr) {
+        int n = arr.length;
+        int[] ans = new int[n];
+        int idx = n - 1;
+        int i = 0, j = n - 1;
+
+        while (i <= j) {
+            if (Math.abs(arr[i]) > Math.abs(arr[j])) {
+                ans[idx--] = arr[i] * arr[i];
+                i++;
+            } else {
+                ans[idx--] = arr[j] * arr[j];
+                j--;
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        Day24_Q1 solution = new Day24_Q1();
+        int[] arr = {-4, -1, 0, 3, 10};
+        int[] result = solution.sortedSquares(arr);
+
+        System.out.print("Sorted squares: ");
+        for (int num : result) {
+            System.out.print(num + " ");
+        }
+    }
+}
