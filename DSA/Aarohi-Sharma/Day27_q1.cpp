d0a9f4ce5f2a3ff266355d8d218eace6b9f65122
Aarohi Sharma <136839094+Aarohi-Sharma@users.noreply.github.com>
2024-07-17 22:54:01 +0530

    Day27_q1.cpp

---
+++ DSA/Aarohi-Sharma/Day27_q1.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int i=0;
+        int n=floor(nums.size()/2);
+        sort(nums.begin(), nums.end());
+        return nums[n];
+    }
+};
