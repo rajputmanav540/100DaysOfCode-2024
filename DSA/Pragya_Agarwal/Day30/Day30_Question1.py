2714f3b984567baca2ef6552a332338189472ee5
Pragya Agarwal <pragyaagarwal560@gmail.com>
2024-07-21 21:49:09 +0530

    Create Day30_Question1.py

---
+++ DSA/Pragya_Agarwal/Day30/Day30_Question1.py
@@ -0,0 +1,24 @@
+class Solution:
+    def maximumProduct(self, nums: List[int]) -> int:
+        max1 = max2 = max3 = float('-inf')
+        min1 = min2 = float('inf')
+        
+        for num in nums:
+            if num > max1:
+                max3 = max2
+                max2 = max1
+                max1 = num
+            elif num > max2:
+                max3 = max2
+                max2 = num
+            elif num > max3:
+                max3 = num
+            if num < min1:
+                min2 = min1
+                min1 = num
+            elif num < min2:
+                min2 = num
+        
+        max_prod1 = max1 * max2 * max3
+        max_prod2 = max1 * min1 * min2
+        return max(max_prod1, max_prod2)
