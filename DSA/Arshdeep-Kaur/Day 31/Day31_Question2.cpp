f7c449660fe00c7d131f7d6134d78ad1a94c44dd
Arshdeep Kaur <76038495+ArshdeepKaur04@users.noreply.github.com>
2024-07-22 21:40:30 +0530

    Create Day31_Question2.cpp

---
+++ DSA/Arshdeep-Kaur/Day 31/Day31_Question2.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int numSubarrayBoundedMax(vector<int>& nums, int left, int right) {
+        int start = -1, end = -1, result = 0;
+        
+        for (int i=0; i<nums.size(); i++) {
+            if (nums[i] > right) {
+                start = end = i;
+                continue;
+            }
+            if (nums[i] >= left) {
+                end = i;
+            }
+            result += end-start;
+        }
+
+        return result;
+    }
+};
