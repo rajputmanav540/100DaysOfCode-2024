b552562f7b87982e3d961d5ca3c4e99375d233ec
Yashvardhan Gulyani <122628663+PeakScripter@users.noreply.github.com>
2024-07-24 11:10:55 +0530

    Create Day33_q1.java

---
+++ DSA/Yashvardhan-Guliyani/Day33_q1.java
@@ -0,0 +1,27 @@
+class Solution {
+
+    public String printString(String s, char ch, int count) {
+
+        for(int i=0;i<s.length();i++)
+
+        {
+
+            if(s.charAt(i)==ch)
+
+            {
+
+                count--;
+
+                if(count==0)
+
+                   return s.substring(i+1);
+
+            }
+
+        }
+
+        return "";
+
+    }
+
+}
