d2391ef77e5a1db18568b03d1b9f6e2f9fddb310
Shruti-Pandeyy <168631862+Shruti-Pandeyy@users.noreply.github.com>
2024-08-01 11:23:13 +0530

    Create Day_41_q2.java

---
+++ DSA/Shruti-Pandey/Day_41_q2.java
@@ -0,0 +1,18 @@
+public int sumSubseqWidths(int[] nums) {
+       long twoPower [] =new long[nums.length];
+       long mod = 1000000007L;
+       twoPower[0] =1;
+       for (int i = 1; i< nums.length; i++){
+           twoPower[i] = (twoPower[i-1]*2)%mod;
+       }
+       long effective[] = new long[nums.length];
+       for (int i = 0; i< nums.length; i++) {
+    	   effective[i] = (twoPower[i]- twoPower[nums.length- i-1]);
+       }
+       Arrays.sort(nums); // sorting the array
+       long ans = 0;
+       for (int i = 0; i< nums.length; i++) {
+    	   ans = (ans+ effective[i]*nums[i])%mod;
+       }
+       return (int) ans;
+   }
