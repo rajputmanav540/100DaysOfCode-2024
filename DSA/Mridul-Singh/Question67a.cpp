366cbac9d1a4421a25bcfc603082414e90256c27
Mridul Singh <91845185+Mridul-Singh-01@users.noreply.github.com>
2024-08-27 15:25:34 +0530

    Day 67 Questions Added

---
+++ DSA/Mridul-Singh/Question67a.cpp
@@ -0,0 +1,24 @@
+//LEETCODE PROBLEM 1130
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int mctFromLeafValues(vector<int>& arr) {
+        int res = 0;
+        vector<int> stack = {INT_MAX};
+        for (int a : arr) {
+            while (stack.back() <= a) {
+                int mid = stack.back();
+                stack.pop_back();
+                res += mid * min(stack.back(), a);
+            }
+            stack.push_back(a);
+        }
+        for (int i = 2; i < stack.size(); ++i) {
+            res += stack[i] * stack[i - 1];
+        }
+        return res;
+    }
+};
